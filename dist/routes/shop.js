b9746b99d505a32185ab0a271a3e791b0684f99c
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-07-25 06:43:47 +0100

    worked on changing the shop pug document title

--- dist/routes/shop.js
+++ dist/routes/shop.js
@@ -3,13 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = __importDefault(require("path"));
 const express_1 = __importDefault(require("express"));
-const path_2 = __importDefault(require("../util/path"));
 const admin_1 = __importDefault(require("./admin"));
 const router = express_1.default.Router();
 router.get("/", (_req, res, _next) => {
-    console.log("Admin products", admin_1.default === null || admin_1.default === void 0 ? void 0 : admin_1.default.products);
-    res.sendFile(path_1.default.join(path_2.default, "..", "views", "shop.html"));
+    const products = admin_1.default.products;
+    // console.log("Admin products", adminData?.products);
+    // res.sendFile(path.join(rootDir, "..", "views", "shop.html"));
+    res.render("shop", { prods: products, docTitle: "Da Shop" });
 });
 exports.default = router;
--- routes/shop.ts
+++ routes/shop.ts
@@ -8,8 +8,10 @@ import adminData from "./admin";
 const router: Router = express.Router();
 
 router.get("/", (_req: Request, res: Response, _next: NextFunction) => {
-  console.log("Admin products", adminData?.products);
-  res.sendFile(path.join(rootDir, "..", "views", "shop.html"));
+  const products = adminData.products;
+  // console.log("Admin products", adminData?.products);
+  // res.sendFile(path.join(rootDir, "..", "views", "shop.html"));
+  res.render("shop", { prods: products, docTitle: "Da Shop" });
 });
 
 export default router;
