09b030c8625de862f872e0f86aab6071a8291188
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-08-03 18:52:20 +0100

    created a helper function to get product from file

--- dist/models/product.js
+++ dist/models/product.js
@@ -8,6 +8,20 @@ const fs_1 = __importDefault(require("fs"));
 const path_2 = __importDefault(require("../util/path"));
 // const products: ProductInterface[] = [];
 const p = path_1.default.join(path_2.default, "..", "data", "products.json");
+const getProductsFromFile = (cb) => {
+    fs_1.default.readFile(p, (err, fileContent) => {
+        console.log("read File", fileContent, fileContent.length);
+        if (err) {
+            cb([]);
+        }
+        else if ((fileContent === null || fileContent === void 0 ? void 0 : fileContent.length) > 0) {
+            cb(JSON.parse(fileContent === null || fileContent === void 0 ? void 0 : fileContent.toString()));
+        }
+        else {
+            cb([]);
+        }
+    });
+};
 class Product {
     constructor(t) {
         this.title = t;
@@ -15,30 +29,25 @@ class Product {
     save() {
         // products.push(this);
         // console.log("console log path", p);
-        fs_1.default.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent.toString());
-            }
+        getProductsFromFile((products) => {
             products.push(this);
             fs_1.default.writeFile(p, JSON.stringify(products), (err) => {
                 console.log("write product error", err);
             });
         });
+        // fs.readFile(p, (err, fileContent) => {
+        //   let products = [];
+        //   if (!err) {
+        //     products = JSON.parse(fileContent.toString());
+        //   }
+        //   products.push(this);
+        //   fs.writeFile(p, JSON.stringify(products), (err) => {
+        //     console.log("write product error", err);
+        //   });
+        // });
     }
     static fetchAll(cb) {
-        let products = [];
-        console.log("List of product ==>", products);
-        fs_1.default.readFile(p, (err, fileContent) => {
-            console.log("Logging ppppp", fileContent, err);
-            if (err) {
-                cb([]);
-            }
-            else if ((fileContent === null || fileContent === void 0 ? void 0 : fileContent.length) > 0) {
-                cb(JSON.parse(fileContent === null || fileContent === void 0 ? void 0 : fileContent.toString()));
-            }
-            cb([]);
-        });
+        getProductsFromFile(cb);
     }
 }
 exports.default = Product;
--- models/product.ts
+++ models/product.ts
@@ -8,6 +8,19 @@ import { Product as ProductInterface } from "../types";
 
 const p = path.join(rootDir, "..", "data", "products.json");
 
+const getProductsFromFile = (cb: Function) => {
+  fs.readFile(p, (err, fileContent) => {
+    console.log("read File", fileContent, fileContent.length);
+    if (err) {
+      cb([]);
+    } else if (fileContent?.length > 0) {
+      cb(JSON.parse(fileContent?.toString()));
+    } else {
+      cb([]);
+    }
+  });
+};
+
 class Product {
   title: string;
   constructor(t: string) {
@@ -18,31 +31,27 @@ class Product {
     // products.push(this);
     // console.log("console log path", p);
 
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent.toString());
-      }
+    getProductsFromFile((products: ProductInterface[]) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log("write product error", err);
       });
     });
+
+    // fs.readFile(p, (err, fileContent) => {
+    //   let products = [];
+    //   if (!err) {
+    //     products = JSON.parse(fileContent.toString());
+    //   }
+    //   products.push(this);
+    //   fs.writeFile(p, JSON.stringify(products), (err) => {
+    //     console.log("write product error", err);
+    //   });
+    // });
   }
 
   static fetchAll(cb: Function) {
-    let products: ProductInterface[] = [];
-
-    console.log("List of product ==>", products);
-    fs.readFile(p, (err, fileContent) => {
-      console.log("Logging ppppp", fileContent, err);
-      if (err) {
-        cb([]);
-      } else if (fileContent?.length > 0) {
-        cb(JSON.parse(fileContent?.toString()));
-      }
-      cb([]);
-    });
+    getProductsFromFile(cb);
   }
 }
 
