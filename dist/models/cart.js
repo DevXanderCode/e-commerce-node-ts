95a9951381d5c195c79d04c5ba89bea49c330a6d
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-08-15 11:45:48 +0100

    started working on the cart model

---
+++ dist/models/cart.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Cart {
+    static addProduct(id) {
+        //TODO: Fetch previous product
+        //TODO: Analyze the cart ==> find existing product
+        //TODO: Add new product / increase quantity
+    }
+}
+;
+exports.default = Cart;
---
+++ models/cart.ts
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import path from 'path'
+
+class Cart {
+    static addProduct(id: string) {
+        //TODO: Fetch previous product
+
+        //TODO: Analyze the cart ==> find existing product
+        //TODO: Add new product / increase quantity
+    }
+};
+
+export default Cart;
