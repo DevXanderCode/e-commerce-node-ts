1f82c28ab464fc5691f117a40bce4228d6568471
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-09-15 11:20:45 +0100

    started working on the cart model with sequalize

--- dist/models/cart.js
+++ dist/models/cart.js
@@ -1,85 +1,96 @@
 "use strict";
+// import fs from "fs";
+// import path from "path";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
-const path_1 = __importDefault(require("path"));
-const path_2 = __importDefault(require("../util/path"));
-const product_1 = __importDefault(require("./product"));
-const p = path_1.default.join(path_2.default, "..", "data", "cart.json");
-const getProductsFromFile = (cb) => {
-    fs_1.default.readFile(p, (err, fileContent) => {
-        // console.log("read File", fileContent, fileContent.length);
-        if (err) {
-            cb([]);
-        }
-        else if ((fileContent === null || fileContent === void 0 ? void 0 : fileContent.length) > 0) {
-            cb(JSON.parse(fileContent === null || fileContent === void 0 ? void 0 : fileContent.toString()));
-        }
-        else {
-            cb([]);
-        }
-    });
-};
-class Cart {
-    static addProduct(id, productPrice) {
-        //Fetch previous product
-        fs_1.default.readFile(p, (err, fileContent) => {
-            let cart = {
-                products: [],
-                totalPrice: 0,
-            };
-            if (!err) {
-                cart = JSON.parse(fileContent === null || fileContent === void 0 ? void 0 : fileContent.toString());
-            }
-            //Analyze the cart ==> find existing product
-            const existingProductIndex = cart.products.findIndex((prod) => (prod === null || prod === void 0 ? void 0 : prod.id) === id);
-            const existingProduct = cart.products[existingProductIndex];
-            let updatedProduct;
-            //Add new product / increase quantity
-            if (existingProduct) {
-                updatedProduct = Object.assign({}, existingProduct);
-                updatedProduct.qty += 1;
-                cart.products = [...cart.products];
-                cart.products[existingProductIndex] = updatedProduct;
-            }
-            else {
-                product_1.default.findById(id, (prod) => {
-                    updatedProduct = Object.assign(Object.assign({}, prod), { qty: 1 });
-                    cart.products = [...cart.products, updatedProduct];
-                    fs_1.default.writeFile(p, JSON.stringify(cart), (err) => {
-                        console.log('write cart error', err);
-                    });
-                });
-            }
-            cart.totalPrice += productPrice;
-            fs_1.default.writeFile(p, JSON.stringify(cart), (err) => {
-                console.log('write cart error', err);
-            });
-        });
-    }
-    static fetchAll(cb) {
-        getProductsFromFile(cb);
-    }
-    static deleteProduct(id) {
-        fs_1.default.readFile(p, (err, fileContent) => {
-            if (err) {
-                return;
-            }
-            const cart = JSON.parse(fileContent.toString());
-            const updatedCart = Object.assign({}, cart);
-            const product = updatedCart.products.find((prod) => (prod === null || prod === void 0 ? void 0 : prod.id) === id);
-            if (!product) {
-                return;
-            }
-            const productQty = product.qty;
-            updatedCart.products = updatedCart.products.filter((prod) => prod.id !== id);
-            updatedCart.totalPrice = updatedCart.totalPrice - ((product === null || product === void 0 ? void 0 : product.price) * productQty);
-            fs_1.default.writeFile(p, JSON.stringify(updatedCart), (err) => {
-                console.log('write cart err', err);
-            });
-        });
-    }
+// import rootDir from "../util/path";
+// import { Product as ProductInterface } from "../types";
+// import Product from "./product";
+// const p = path.join(rootDir, "..", "data", "cart.json");
+// const getProductsFromFile = (cb: Function) => {
+//     fs.readFile(p, (err, fileContent) => {
+//       // console.log("read File", fileContent, fileContent.length);
+//       if (err) {
+//         cb([]);
+//       } else if (fileContent?.length > 0) {
+//         cb(JSON.parse(fileContent?.toString()));
+//       } else {
+//         cb([]);
+//       }
+//     });
+//   };
+// class Cart {
+//   static addProduct(id: string, productPrice: number) {
+//     //Fetch previous product
+//     fs.readFile(p, (err, fileContent) => {
+//       let cart: { products: ProductInterface[]; totalPrice: number } = {
+//         products: [],
+//         totalPrice: 0,
+//       };
+//       if (!err) {
+//         cart = JSON.parse(fileContent?.toString());
+//       }
+//       //Analyze the cart ==> find existing product
+//       const existingProductIndex = cart.products.findIndex((prod) => prod?.id === id);
+//       const existingProduct = cart.products[existingProductIndex];
+//       let updatedProduct: ProductInterface;
+//       //Add new product / increase quantity
+//       if (existingProduct) {
+//         updatedProduct = { ...existingProduct };
+//         updatedProduct.qty! += 1;
+//         cart.products = [...cart.products];
+//         cart.products[existingProductIndex] = updatedProduct;
+//       } else {
+//         Product.findById(id, (prod: ProductInterface) => {
+//             updatedProduct = { ...prod,qty: 1 };
+//             cart.products = [...cart.products, updatedProduct];
+//             fs.writeFile(p, JSON.stringify(cart), (err) => {
+//                 console.log('write cart error', err)
+//               })
+//         })
+//       }
+//      cart.totalPrice += productPrice;
+//       fs.writeFile(p, JSON.stringify(cart), (err) => {
+//         console.log('write cart error', err)
+//       })
+//     });
+//   }
+//   static fetchAll(cb: Function) {
+//     getProductsFromFile(cb);
+//   }
+//   static deleteProduct (id: string) {
+//     fs.readFile(p, (err, fileContent) => {
+//       if (err) {
+//         return;
+//       }
+//       const cart = JSON.parse(fileContent.toString());
+//       const updatedCart = {...cart};
+//       const product = updatedCart.products.find((prod: ProductInterface) => prod?.id === id);
+//       if (!product) {
+//         return;
+//       }
+//       const productQty = product.qty;
+//       updatedCart.products = updatedCart.products.filter((prod: ProductInterface) => prod.id !== id);
+//       updatedCart.totalPrice = updatedCart.totalPrice - (product?.price * productQty);
+//       fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+//         console.log('write cart err', err)
+//       })
+//     })
+//   }
+// }
+// export default Cart;
+const sequelize_1 = require("sequelize");
+const database_1 = __importDefault(require("../util/database"));
+class Cart extends sequelize_1.Model {
 }
+Cart.init({
+    id: {
+        type: sequelize_1.DataTypes.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+}, { sequelize: database_1.default, tableName: "cart" });
 exports.default = Cart;
--- models/cart.ts
+++ models/cart.ts
@@ -1,86 +1,113 @@
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
 
-import rootDir from "../util/path";
-import { Product as ProductInterface } from "../types";
-import Product from "./product";
+// import rootDir from "../util/path";
+// import { Product as ProductInterface } from "../types";
+// import Product from "./product";
 
-const p = path.join(rootDir, "..", "data", "cart.json");
+// const p = path.join(rootDir, "..", "data", "cart.json");
 
-const getProductsFromFile = (cb: Function) => {
-    fs.readFile(p, (err, fileContent) => {
-      // console.log("read File", fileContent, fileContent.length);
-      if (err) {
-        cb([]);
-      } else if (fileContent?.length > 0) {
-        cb(JSON.parse(fileContent?.toString()));
-      } else {
-        cb([]);
-      }
-    });
-  };
+// const getProductsFromFile = (cb: Function) => {
+//     fs.readFile(p, (err, fileContent) => {
+//       // console.log("read File", fileContent, fileContent.length);
+//       if (err) {
+//         cb([]);
+//       } else if (fileContent?.length > 0) {
+//         cb(JSON.parse(fileContent?.toString()));
+//       } else {
+//         cb([]);
+//       }
+//     });
+//   };
 
-class Cart {
-  static addProduct(id: string, productPrice: number) {
-    //Fetch previous product
-    fs.readFile(p, (err, fileContent) => {
-      let cart: { products: ProductInterface[]; totalPrice: number } = {
-        products: [],
-        totalPrice: 0,
-      };
-      if (!err) {
-        cart = JSON.parse(fileContent?.toString());
-      }
-      //Analyze the cart ==> find existing product
-      const existingProductIndex = cart.products.findIndex((prod) => prod?.id === id);
-      const existingProduct = cart.products[existingProductIndex];
-      let updatedProduct: ProductInterface;
-      //Add new product / increase quantity
-      if (existingProduct) {
-        updatedProduct = { ...existingProduct };
-        updatedProduct.qty! += 1;
-        cart.products = [...cart.products];
-        cart.products[existingProductIndex] = updatedProduct;
-      } else {
-        Product.findById(id, (prod: ProductInterface) => {
-            updatedProduct = { ...prod,qty: 1 };
-            cart.products = [...cart.products, updatedProduct];
-            fs.writeFile(p, JSON.stringify(cart), (err) => {
-                console.log('write cart error', err)
-              })
-        })
-      }
-     cart.totalPrice += productPrice;
-      fs.writeFile(p, JSON.stringify(cart), (err) => {
-        console.log('write cart error', err)
-      })
-    });
-  }
+// class Cart {
+//   static addProduct(id: string, productPrice: number) {
+//     //Fetch previous product
+//     fs.readFile(p, (err, fileContent) => {
+//       let cart: { products: ProductInterface[]; totalPrice: number } = {
+//         products: [],
+//         totalPrice: 0,
+//       };
+//       if (!err) {
+//         cart = JSON.parse(fileContent?.toString());
+//       }
+//       //Analyze the cart ==> find existing product
+//       const existingProductIndex = cart.products.findIndex((prod) => prod?.id === id);
+//       const existingProduct = cart.products[existingProductIndex];
+//       let updatedProduct: ProductInterface;
+//       //Add new product / increase quantity
+//       if (existingProduct) {
+//         updatedProduct = { ...existingProduct };
+//         updatedProduct.qty! += 1;
+//         cart.products = [...cart.products];
+//         cart.products[existingProductIndex] = updatedProduct;
+//       } else {
+//         Product.findById(id, (prod: ProductInterface) => {
+//             updatedProduct = { ...prod,qty: 1 };
+//             cart.products = [...cart.products, updatedProduct];
+//             fs.writeFile(p, JSON.stringify(cart), (err) => {
+//                 console.log('write cart error', err)
+//               })
+//         })
+//       }
+//      cart.totalPrice += productPrice;
+//       fs.writeFile(p, JSON.stringify(cart), (err) => {
+//         console.log('write cart error', err)
+//       })
+//     });
+//   }
 
-  static fetchAll(cb: Function) {
-    getProductsFromFile(cb);
-  }
+//   static fetchAll(cb: Function) {
+//     getProductsFromFile(cb);
+//   }
 
-  static deleteProduct (id: string) {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        return;
-      }
-      const cart = JSON.parse(fileContent.toString());
-      const updatedCart = {...cart};
-      const product = updatedCart.products.find((prod: ProductInterface) => prod?.id === id);
-      if (!product) {
-        return;
-      }
-      const productQty = product.qty;
-      updatedCart.products = updatedCart.products.filter((prod: ProductInterface) => prod.id !== id);
-      updatedCart.totalPrice = updatedCart.totalPrice - (product?.price * productQty);
+//   static deleteProduct (id: string) {
+//     fs.readFile(p, (err, fileContent) => {
+//       if (err) {
+//         return;
+//       }
+//       const cart = JSON.parse(fileContent.toString());
+//       const updatedCart = {...cart};
+//       const product = updatedCart.products.find((prod: ProductInterface) => prod?.id === id);
+//       if (!product) {
+//         return;
+//       }
+//       const productQty = product.qty;
+//       updatedCart.products = updatedCart.products.filter((prod: ProductInterface) => prod.id !== id);
+//       updatedCart.totalPrice = updatedCart.totalPrice - (product?.price * productQty);
 
-      fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
-        console.log('write cart err', err)
-      })
-    })
-  }
+//       fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+//         console.log('write cart err', err)
+//       })
+//     })
+//   }
+// }
+
+// export default Cart;
+
+import {
+  CreationOptional,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from "sequelize";
+import sequelize from "../util/database";
+
+class Cart extends Model<InferAttributes<Cart>, InferCreationAttributes<Cart>> {
+  declare id: CreationOptional<number>;
 }
+
+Cart.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+  },
+  { sequelize, tableName: "cart" }
+);
 
 export default Cart;
